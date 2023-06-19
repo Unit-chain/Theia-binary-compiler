de717d2e9396be928d74cee862f613e6a7c87bdf
g2px1 <kir1kir1@yandex.ru>
2023-06-19 23:43:55 +0300

    Update Opcodes.h, add opcodes validation process

--- binaryWriter/Opcodes.h
+++ binaryWriter/Opcodes.h
@@ -7,6 +7,8 @@
 #include "unordered_map"
 #include "IR/IRCommandParser.h"
 
+namespace theiaIR {
+
 struct ReferenceOpcode {
     u1 args;
     u1 index;
@@ -17,63 +19,64 @@ struct ReferenceOpcode {
 ///@param op unordered map which contains ReferenceOpcode by it's name as a key
 class Opcodes {
 public:
-    explicit Opcodes();
     ///@brief checks if opcode is allowed
     ///@param cmd parsed command
     bool containsOpcode(Command &cmd);
+
     ///@brief return index by parsed command
     ///@param cmd parsed command
     u1 getIndexByCommand(Command &cmd);
+
     ///@brief returns reference opcode by name
     ///@param cmdName command name
     ReferenceOpcode getReferenceCommand(std::string &cmdName);
+
     ///@brief checks if command is valid
     ///@param cmd parsed command
     bool validateCommand(Command &cmd);
-private:
-    std::unordered_map<std::string, ReferenceOpcode> op;
-};
 
-Opcodes::Opcodes() {
-    this->op["stop"]        = {.args = 0, .index = 0};
-    this->op["bl"]          = {.args = 1, .index = 1};
-    this->op["swap"]        = {.args = 2, .index = 2};
-    this->op["add"]         = {.args = 2, .index = 3, .allowedFlags = 'v'};
-    this->op["sub"]         = {.args = 2, .index = 4, .allowedFlags = 'v'};
-    this->op["div"]         = {.args = 2, .index = 5, .allowedFlags = 'v'};
-    this->op["mul"]         = {.args = 2, .index = 6, .allowedFlags = 'v'};
-    this->op["mod"]         = {.args = 2, .index = 7, .allowedFlags = 'v'};
-    this->op["or"]          = {.args = 2, .index = 8, .allowedFlags = 'v'};
-    this->op["and"]         = {.args = 2, .index = 9, .allowedFlags = 'v'};
-    this->op["xor"]         = {.args = 2, .index = 10, .allowedFlags = 'v'};
-    this->op["lshift"]      = {.args = 2, .index = 11, .allowedFlags = 'v'};
-    this->op["rshift"]      = {.args = 2, .index = 12, .allowedFlags = 'v'};
-    this->op["pop"]         = {.args = 1, .index = 13};
-    this->op["pop2"]        = {.args = 2, .index = 14};
-    this->op["dup"]         = {.args = 1, .index = 15};
-    this->op["vmcall"]      = {.args = 1, .index = 16};
-    this->op["cnst_0"]      = {.args = 1, .index = 17};
-    this->op["cnst_1"]      = {.args = 1, .index = 18};
-    this->op["cnst_2"]      = {.args = 1, .index = 19};
-    this->op["cnst_3"]      = {.args = 1, .index = 20};
-    this->op["cnst_4"]      = {.args = 1, .index = 21};
-    this->op["astorea"]     = {.args = 1, .index = 22};
-    this->op["aloada"]      = {.args = 1, .index = 23};
-    this->op["str"]         = {.args = 1, .index = 24};
-    this->op["ldr"]         = {.args = 1, .index = 25};
-    this->op["strp"]        = {.args = 2, .index = 26};
-    this->op["cmp_eq"]      = {.args = 3, .index = 27, .allowedFlags = 'v'};
-    this->op["cmp_ne"]      = {.args = 3, .index = 28, .allowedFlags = 'v'};
-    this->op["cmp_ge"]      = {.args = 3, .index = 29, .allowedFlags = 'v'};
-    this->op["cmp_le"]      = {.args = 3, .index = 30, .allowedFlags = 'v'};
-    this->op["cmp_gt"]      = {.args = 3, .index = 31, .allowedFlags = 'v'};
-    this->op["cmp_lt"]      = {.args = 3, .index = 32, .allowedFlags = 'v'};
-    this->op["nop"]         = {.args = 0, .index = 33};
-    this->op["call"]        = {.args = 2, .index = 34};
-    this->op["ret"]         = {.args = 1, .index = 35};
-    this->op["virtualcall"] = {.args = 1, .index = 36};
-    this->op["align"]       = {.args = 1, .index = 37};
-}
+private:
+    const std::unordered_map<std::string, ReferenceOpcode> op = {
+            {"stop",        {.args = 0, .index = 0}},
+            {"bl",          {.args = 1, .index = 1}},
+            {"swap",        {.args = 2, .index = 2}},
+            {"add",         {.args = 2, .index = 3, .allowedFlags = 'v'}},
+            {"sub",         {.args = 2, .index = 4, .allowedFlags = 'v'}},
+            {"div",         {.args = 2, .index = 5, .allowedFlags = 'v'}},
+            {"mul",         {.args = 2, .index = 6, .allowedFlags = 'v'}},
+            {"mod",         {.args = 2, .index = 7, .allowedFlags = 'v'}},
+            {"or",          {.args = 2, .index = 8, .allowedFlags = 'v'}},
+            {"and",         {.args = 2, .index = 9, .allowedFlags = 'v'}},
+            {"xor",         {.args = 2, .index = 10, .allowedFlags = 'v'}},
+            {"lshift",      {.args = 2, .index = 11, .allowedFlags = 'v'}},
+            {"rshift",      {.args = 2, .index = 12, .allowedFlags = 'v'}},
+            {"pop",         {.args = 1, .index = 13}},
+            {"pop2",        {.args = 2, .index = 14}},
+            {"dup",         {.args = 1, .index = 15}},
+            {"vmcall",      {.args = 1, .index = 16}},
+            {"cnst_0",      {.args = 1, .index = 17}},
+            {"cnst_1",      {.args = 1, .index = 18}},
+            {"cnst_2",      {.args = 1, .index = 19}},
+            {"cnst_3",      {.args = 1, .index = 20}},
+            {"cnst_4",      {.args = 1, .index = 21}},
+            {"astorea",     {.args = 1, .index = 22}},
+            {"aloada",      {.args = 1, .index = 23}},
+            {"str",         {.args = 1, .index = 24}},
+            {"ldr",         {.args = 1, .index = 25}},
+            {"strp",        {.args = 2, .index = 26}},
+            {"cmp_eq",      {.args = 3, .index = 27, .allowedFlags = 'v'}},
+            {"cmp_ne",      {.args = 3, .index = 28, .allowedFlags = 'v'}},
+            {"cmp_ge",      {.args = 3, .index = 29, .allowedFlags = 'v'}},
+            {"cmp_le",      {.args = 3, .index = 30, .allowedFlags = 'v'}},
+            {"cmp_gt",      {.args = 3, .index = 31, .allowedFlags = 'v'}},
+            {"cmp_lt",      {.args = 3, .index = 32, .allowedFlags = 'v'}},
+            {"nop",         {.args = 0, .index = 33}},
+            {"call",        {.args = 2, .index = 34}},
+            {"ret",         {.args = 1, .index = 35}},
+            {"virtualcall", {.args = 1, .index = 36}},
+            {"align",       {.args = 1, .index = 37}}
+    };
+};
 
 bool Opcodes::containsOpcode(Command &cmd) {
     if (validateCommand(cmd)) {
@@ -95,11 +98,12 @@ ReferenceOpcode Opcodes::getReferenceCommand(std::string &cmdName) {
 
 bool Opcodes::validateCommand(Command &cmd) {
     ReferenceOpcode opcode = this->op.at(cmd.name);
-    if ((cmd.args.size() == opcode.args) && (cmd.flag == opcode.allowedFlags)) [[likely]] {
+    if ((cmd.args.size() == opcode.args) && ((cmd.flag == opcode.allowedFlags) || (cmd.flag == '\0'))) [[likely]] {
         return true;
     } else [[unlikely]] {
         return false;
     }
+}
 }
 
 #endif //BINARYWRITER_OPCODES_H
